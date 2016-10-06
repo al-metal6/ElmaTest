be709677b50fb4caae653463c8e93b29b580c16c
al-metal <al-metal@bk.ru>
2016-10-06 14:38:51 +0400

    Форма калькулятора
    изменены все имена компонентов и свойство текст.
    Минимальный размер формы и закреплены часть элементов

--- ElmaTest/Form1.Designer.cs
+++ ElmaTest/Form1.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnActions = new System.Windows.Forms.Button();
+            this.rtbHistory = new System.Windows.Forms.RichTextBox();
+            this.gbActions = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(12, 12);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(100, 20);
+            this.tbX.TabIndex = 0;
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(118, 12);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(100, 20);
+            this.tbY.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(230, 13);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 2;
+            // 
+            // btnActions
+            // 
+            this.btnActions.Location = new System.Drawing.Point(289, 10);
+            this.btnActions.Name = "btnActions";
+            this.btnActions.Size = new System.Drawing.Size(75, 23);
+            this.btnActions.TabIndex = 3;
+            this.btnActions.Text = "Выполнить";
+            this.btnActions.UseVisualStyleBackColor = true;
+            // 
+            // rtbHistory
+            // 
+            this.rtbHistory.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtbHistory.Location = new System.Drawing.Point(0, 146);
+            this.rtbHistory.Name = "rtbHistory";
+            this.rtbHistory.Size = new System.Drawing.Size(390, 138);
+            this.rtbHistory.TabIndex = 4;
+            this.rtbHistory.Text = "";
+            // 
+            // gbActions
+            // 
+            this.gbActions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbActions.Location = new System.Drawing.Point(12, 38);
+            this.gbActions.Name = "gbActions";
+            this.gbActions.Size = new System.Drawing.Size(366, 102);
+            this.gbActions.TabIndex = 5;
+            this.gbActions.TabStop = false;
+            this.gbActions.Text = "Выберите действие";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(390, 284);
+            this.Controls.Add(this.gbActions);
+            this.Controls.Add(this.rtbHistory);
+            this.Controls.Add(this.btnActions);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX);
+            this.MinimumSize = new System.Drawing.Size(406, 323);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Elma калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnActions;
+        private System.Windows.Forms.RichTextBox rtbHistory;
+        private System.Windows.Forms.GroupBox gbActions;
     }
 }
 
