1f028ad2d7727ed0c2c9357d0a44cc67579855c5
al-metal <al-metal@bk.ru>
2016-10-06 15:40:29 +0400

    Рабочий калькулятор на форме
    Создает радиобтн на каждое действие, считает, записывает историю,
    сохраняет и подгружает историю

--- Calc/Helper.cs
+++ Calc/Helper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,6 +34,12 @@ namespace Calc
         public double Step(double x, double y)
         {
             return Math.Pow(x, y);
+        }
+
+        public MethodInfo[] metods()
+        {
+            var methods = GetType().GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+            return methods;
         }
     }
 }
--- ElmaTest/Form1.cs
+++ ElmaTest/Form1.cs
@@ -1,31 +1,83 @@
 ﻿using Calc;
 using System;
 using System.Windows.Forms;
+using HistoryFile;
 
 namespace ElmaTest
 {
     public partial class Form1 : Form
     {
         private Helper Calc { get; set; }
-
-
+        private string ActiveOperation { get; set; }
+        private History HistoryFiles { get; set; }
+        
         public Form1()
         {
             InitializeComponent();
+            Calc = new Helper();
+            HistoryFiles = new History();
+            var methods = Calc.metods();
+            var count = 0;
+            gbActions.SuspendLayout();
+
+            foreach (var m in methods)
+            {
+                CreateRadioButton(m.Name, count++);
+            }
+
+            gbActions.ResumeLayout();
+            HistoryFiles.HistoryLoad(ref rtbHistory);
+        }
+
+        private void CreateRadioButton(string Name, int index)
+        {
+            var rbBtn = new RadioButton();
+            gbActions.Controls.Add(rbBtn);
+            
+            rbBtn.AutoSize = true;
+            rbBtn.Location = new System.Drawing.Point(7, 15 + index * 18);
+            rbBtn.Name = "rbBtn" + index.ToString();
+            rbBtn.Size = new System.Drawing.Size(53, 17);
+            rbBtn.TabIndex = 5;
+            rbBtn.TabStop = true;
+            rbBtn.Tag = Name;
+            rbBtn.Text = Name;
+            rbBtn.UseVisualStyleBackColor = true;
+            rbBtn.CheckedChanged += new EventHandler(radio_CheckedChanged);
         }
 
         private void btnActions_Click(object sender, EventArgs e)
         {
-            Calc = new Helper();
             double x = 0;
             double y = 0;
-            double.TryParse(tbX.Text, out x);
-            double.TryParse(tbY.Text, out y);
 
-            var result = Calc.Sum(x, y);
-            Calc.Sum(x, y);
+            if (!double.TryParse(tbX.Text, out x) || !double.TryParse(tbY.Text, out y) || ActiveOperation == null)
+            {
+                rtbHistory.Text += string.Format("При вводе данных была ошибка " + Environment.NewLine);
+                return;
+            }
+
+            var calcType = Calc.GetType();
+            var method = calcType.GetMethod(ActiveOperation);
+            var result = method.Invoke(Calc, new object[] { x, y });
+
             lblResult.Text = result.ToString();
+            rtbHistory.Text += string.Format("{0} {1} {2} = {3}{4}", x, ActiveOperation, y, result, Environment.NewLine);
+        }
 
+        private void radio_CheckedChanged(object sender, EventArgs e)
+        {
+            var rb = sender as RadioButton;
+            if (rb == null)
+            {
+                return;
+            }
+            ActiveOperation = rb.Tag.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            HistoryFiles.HistorySave(ref rtbHistory);
         }
     }
 }
